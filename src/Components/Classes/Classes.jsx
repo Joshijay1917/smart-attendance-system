dedf3c9b42f553aea1acc42189eaca04d67dbd72
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-09-18 09:44:15 +0530

    Update Classes.jsx

--- src/Components/Classes/Classes.jsx
+++ src/Components/Classes/Classes.jsx
@@ -5,10 +5,12 @@ const Classes = () => {
   return (
     <div className='box p-3 my-5'>
       <h1 className='heading'>Today's Classes</h1>
+      <div className='grid grid-cols-2'>
       <Class />
       <Class />
       <Class />
       <Class />
+        </div>
     </div>
   )
 }
