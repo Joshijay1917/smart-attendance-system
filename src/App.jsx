e9a55555c1e958569c9c1d985ad96468e9373c90
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-09-18 09:46:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='mx-20'>
     <Header />
     <QuickAccess />
     <Overview />
     <Classes />
-    </>
+    </div>
   )
 }
 
