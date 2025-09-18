dbda0ffe598d51a51cb7f170375ab5a6d7a52786
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-09-18 20:00:20 +0530

    Update Graph.jsx

--- src/Components/Report/charts/Graph.jsx
+++ src/Components/Report/charts/Graph.jsx
@@ -1,31 +1,5 @@
 import React from 'react'
 import { Chart } from "react-chartjs-2";
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    BarElement,
-    ArcElement,
-    Title,
-    Tooltip,
-    Legend,
-    Colors
-  } from "chart.js";
-  
-  ChartJS.register(
-      CategoryScale,
-      LinearScale,
-      PointElement,
-      LineElement,
-      ArcElement,
-      BarElement,
-      Title,
-      Tooltip,
-      Legend,
-      Colors
-    );
 
 const Graph = ({type, data, options}) => {
   return (
