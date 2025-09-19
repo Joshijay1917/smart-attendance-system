8e3f414ba74c5b128301fc14d6c689663752c723
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-09-19 22:45:19 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,22 +4,22 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-console.log("Key=",process.env.VITE_FIREBASE_API_KEY);
-console.log("AuthDoma=",process.env.VITE_FIREBASE_AUTH_DOMAIN);
-console.log("ID=",process.env.VITE_FIREBASE_PROJECT_ID);
-console.log("storage=",process.env.VITE_FIREBASE_STORAGE_BUCKET);
-console.log("sender=",process.env.VITE_FIREBASE_MESSAGING_SENDER_ID);
-console.log("app=",process.env.VITE_FIREBASE_APP_ID);
+console.log("Key=",import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("AuthDoma=",import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
+console.log("ID=",import.meta.env.VITE_FIREBASE_PROJECT_ID);
+console.log("storage=",import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
+console.log("sender=",import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID);
+console.log("app=",import.meta.env.VITE_FIREBASE_APP_ID);
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
-  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-VJ7TTC7TH0"
 };
 
