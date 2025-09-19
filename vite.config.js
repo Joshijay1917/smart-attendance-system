93503acfe4be7c352e771dba85d2a12896dec467
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-09-19 07:51:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true, // This is important for development
+  },
 })
