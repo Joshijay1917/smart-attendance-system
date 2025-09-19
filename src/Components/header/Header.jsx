45303acf270fee9533134d26e7d616fd3545b802
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-09-19 08:14:57 +0530

    Update Header.jsx

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <p>Manage your classes and monitor attendance in real-time</p>
       <div className='flex gap-3'>
       <button onClick={()=>window.location.href = '/'} className='btn my-3 flex items-center gap-2'><SlRefresh className='text-[24px]'/> Refresh</button>
-      {/* <input type='date' value={new Date().toISOString().split("T")[0]}/> */}
+       <input type='date' value={new Date().toISOString().split("T")[0]}/> 
       </div>
     </div>
   )
