678ba2a53f5886e2fac01c6e00547c2cccc6ff98
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-09-19 22:40:22 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,6 +5,8 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+console.log("ID=",process.env.VITE_FIREBASE_PROJECT_ID);
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
