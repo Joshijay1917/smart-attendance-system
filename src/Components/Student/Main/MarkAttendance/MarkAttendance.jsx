2e4b12954c099029b579b003d12ef97ec3abc179
Jay <jayjoshi1912007@gmail.com>
2025-09-21 20:12:58 +0530

    add backend scan call to verify token

--- src/Components/Student/Main/MarkAttendance/MarkAttendance.jsx
+++ src/Components/Student/Main/MarkAttendance/MarkAttendance.jsx
@@ -6,19 +6,61 @@ const MarkAttendance = () => {
     const videoRef = useRef();
     const [errMsg, seterrMsg] = useState('')
     const [ZoomLevel, setZoomLevel] = useState(1)
+    const [loading, setloading] = useState(false)
+    const [disableScanner, setdisableScanner] = useState(false)
 
     const onUpdate = useCallback(({ x, y, scale }) => {
         const { current: video } = videoRef;
-    
-        if (video) {
-          const value = make3dTransformValue({ x, y, scale });
-    
-          video.style.setProperty("transform", value);
-        }
-      }, []);
 
-    const handleScan = (data) => {
+        if (video) {
+            const value = make3dTransformValue({ x, y, scale });
+
+            video.style.setProperty("transform", value);
+        }
+    }, []);
+
+    const isTokenValid = async (token) => {
+        if (!token) return false;
+
+        try {
+            const strRes = await fetch('https://check-ip-test-backend.onrender.com/api/v1/qr/scan', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ token: token, userId: username })
+            });
+            const res = await strRes.json()
+
+            console.log("Res:", res);
+            if (res.success) {
+                return true;
+            }
+
+            return false;
+        } catch (error) {
+            return false;
+        }
+    };
+
+    const handleScan = async (data) => {
+        setloading(true)
         console.log("Data ", data);
+        if (data[0].rawValue) {
+            try {
+                if (!(await isTokenValid(data[0].rawValue))) {
+                    console.log("Attendance Not marked")
+                    setloading("Failed to mark Attendance");
+                    return;
+                }
+                setloading("Attendance Marked")
+            } catch (error) {
+                setloading("Err")
+                console.log("Err ", error);
+            }
+        } else {
+            setloading('No Data Found')
+        }
     }
 
     const handleError = (err) => {
@@ -38,13 +80,17 @@ const MarkAttendance = () => {
         <div className='box p-3 mt-3'>
             <h1 className='heading-3'>Mark Attendance</h1>
 
-            <QuickPinchZoom onUpdate={onUpdate}>
-            <Scanner
-                onError={handleError}
-                onScan={handleScan}
-                ref={videoRef}
-            />
+            {!disableScanner ? <QuickPinchZoom onUpdate={onUpdate}>
+                <Scanner
+                    onError={handleError}
+                    onScan={handleScan}
+                    ref={videoRef}
+                />
             </QuickPinchZoom>
+            :
+            <div className='text-center'>
+                <p>Only One Time Scan is Allowed!</p>
+            </div>}
 
             <div>
                 <label htmlFor="zoom">Zoom:</label>
@@ -58,6 +104,9 @@ const MarkAttendance = () => {
                 />
             </div>
             {errMsg && <p className='text-red-500'>{errMsg}</p>}
+            {loading && <div className='bg-black/50 fixed left-0 top-0 w-full min-h-screen flex justify-center items-center text-white font-bold text-3xl'>
+                <p>{loading}</p>
+            </div>}
         </div>
     )
 }
