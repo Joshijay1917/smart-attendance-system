554fa3aa0a08cb53418d6cb19be08e03e6d07a74
Jay <jayjoshi1912007@gmail.com>
2025-09-21 15:53:21 +0530

    update header

--- src/Components/Student/header/StuHeader.jsx
+++ src/Components/Student/header/StuHeader.jsx
@@ -8,7 +8,7 @@ const StuHeader = ({username}) => {
                 <h1 className='heading flex flex-col'>Welcom Back!, <p>{username.name}</p></h1>
                 <p>Mark your attendance and see analytics in real-time</p>
             </div>
-            <div className='flex gap-3'>
+            <div className='flex gap-3 items-center'>
                 <button onClick={() => window.location.href = '/'} className='btn my-3 flex items-center gap-2'><SlRefresh className='text-[24px]' /> Refresh</button>
                 <div className='hidden md:inline'>
                     <span>Date:</span>
